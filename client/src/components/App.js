39d26d93f6d50a2f349b448e1bd69c473ce6db02
Mathieu Szydlowski <mathieu.lowski@gmail.com>
2020-08-07 16:03:43 -0400

    fixesd

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Header from "./Header";
 
 // HELLLLOOOOO
 
@@ -11,7 +12,12 @@ function App() {
       .then((data) => setBacon(data));
   }, []);
 
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  return (
+    <>
+      <Header />
+      <div>{bacon ? bacon : `...where's my stuff?...`}</div>
+    </>
+  );
 }
 
 export default App;
