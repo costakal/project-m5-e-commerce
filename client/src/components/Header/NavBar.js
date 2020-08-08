3dd224262067655ba018b5b3736aebd17ba80a76
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-08-08 14:44:04 -0400

    Started work on responsive header - near complete

--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -1,14 +1,57 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+import hamburger from "../../assets/hamburger.png";
 
 const NavBar = () => {
   return (
-    <div>
-      <button>Products</button>
-      <h1>Unknown store name</h1>
+    <Wrapper>
+      <MobileNavMenu>
+        <img src={hamburger} />
+        <NavLink to="/">Products</NavLink>
+        <NavLink to="/">Companies</NavLink>
+      </MobileNavMenu>
+      <WebNavMenu>
+        <NavLink to="/">Products</NavLink>
+        <NavLink to="/">Companies</NavLink>
+      </WebNavMenu>
       <button>Cart</button>
-    </div>
+    </Wrapper>
   );
 };
 
 export default NavBar;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    margin: 20px 40px;
+  }
+  a {
+    padding: 0px 40px;
+  }
+`;
+
+const MobileNavMenu = styled.div`
+  img {
+    height: 35px;
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+  a {
+    display: none;
+  }
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const WebNavMenu = styled.div`
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
 import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <div>
+    <Wrapper>
+      <StoreName>The Store Name</StoreName>
       <NavBar />
-    </div>
+    </Wrapper>
   );
 };
 
 export default Header;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  border-bottom: 1px solid black;
+`;
+
+const StoreName = styled.h1`
+  padding: 20px;
+  text-align: center;
+`;
