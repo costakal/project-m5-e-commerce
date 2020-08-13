489cf87669370c7aa49515f22ced625a34271b7e
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-08-13 12:00:41 -0400

    Styled ItemDetails

--- client/src/components/Item/ItemDetails.js
+++ client/src/components/Item/ItemDetails.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import styled from "styled-components";
 
 import Loading from "../Loading";
-import { HEADER_HEIGHT } from "../../constants";
+import { HEADER_HEIGHT, COLORS, FONT_STYLES } from "../../constants";
 import { useSelector, useDispatch } from "react-redux";
 import {
   addItemToCart,
   toggleCartModal,
   addExistingItemToCart,
 } from "../../actions";
+import UnstyledButton from "../UnstyledButton";
 
 const ItemDetails = () => {
   const dispatch = useDispatch();
   const cartItems = useSelector((state) => state.cartReducer.cartItems);
   const items = useSelector((state) => state.itemsReducer.items);
+  const companies = useSelector((state) => state.companiesReducer.companies);
   const { itemId } = useParams();
   const [itemData, setItemData] = useState(null);
 
@@ -33,14 +35,46 @@ const ItemDetails = () => {
     }
   }, [items]);
 
-  console.log(cartItems);
+  const getCompanyName = () => {
+    return companies.companies.find(
+      (element) => element._id === itemData.companyId
+    ).name;
+  };
+
+  const getCompanyUrl = () => {
+    return companies.companies.find(
+      (element) => element._id === itemData.companyId
+    ).url;
+  };
+
+  console.log(companies);
   return (
     <>
       {itemData ? (
         <Wrapper>
-          <h2>{itemData.name}</h2>
-          <img src={itemData.imageSrc} alt={itemData.name} />
-          <button onClick={() => handleAddToCart(itemData)}>Add to cart</button>
+          <LeftSide>
+            <h2>{itemData.name}</h2>
+            <p>Sold by: {getCompanyName()}</p>
+            <p>Website: {getCompanyUrl()}</p>
+            <Purchase>
+              <p>{itemData.numInStock} left in stock </p>
+              <CartButton onClick={() => handleAddToCart(itemData)}>
+                Add to cart
+              </CartButton>
+            </Purchase>
+            <Tags>
+              <p>Tags:</p>
+              <span>
+                <Link to={`/categories/${itemData.category}`}>
+                  {itemData.category}
+                </Link>
+              </span>
+              <span>{itemData.body_location}</span>
+            </Tags>
+          </LeftSide>
+          <RightSide>
+            <img src={itemData.imageSrc} alt={itemData.name} />
+          </RightSide>
         </Wrapper>
       ) : (
         <Loading />
@@ -50,8 +84,69 @@ const ItemDetails = () => {
 };
 
 const Wrapper = styled.div`
-  position: absolute;
-  top: ${HEADER_HEIGHT};
+  padding: ${HEADER_HEIGHT} 50px 0px;
+  display: grid;
+  grid-template-columns: 40% 60%;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+`;
+
+const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 40px 0px;
+  h2 {
+    font-size: 28px;
+    padding-bottom: 10px;
+  }
+  p {
+    font-size: 16px;
+  }
+`;
+
+const CartButton = styled(UnstyledButton)`
+  font-size: 16px;
+  margin: 10px 0px 0px;
+  padding: 12px;
+  color: black;
+  border: solid black 3px;
+  width: 100%;
+  transition: 0.15s;
+  text-align: center;
+  &:hover {
+    background-color: ${COLORS.primary};
+    border: solid ${COLORS.primary} 3px;
+    color: white;
+  }
+`;
+
+const Purchase = styled.div`
+  padding: 40px 0px;
+  width: 100%;
+`;
+
+const Tags = styled.div`
+  p {
+    font-size: 16px;
+    padding-bottom: 15px;
+  }
+  span {
+    font-size: 14px;
+    background-color: ${COLORS.border};
+    margin: 0px 5px 0px 0px;
+    padding: 8px;
+  }
+`;
+
+const RightSide = styled.div`
+  display: flex;
+  justify-content: center;
+  img {
+    padding: 50px 200px 85px;
+    width: 100%;
+  }
 `;
 
 export default ItemDetails;
