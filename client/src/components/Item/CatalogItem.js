1a4af2530d5aa7320ed08f1b4d4b18dc880dd33a
johnnytortorici <johnny.tortorici@gmail.com>
2020-08-13 12:10:59 -0400

    CatalogItem styling and linking

--- client/src/components/Item/CatalogItem.js
+++ client/src/components/Item/CatalogItem.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { useSelector } from "react-redux";
 import Loading from "../Loading";
 
+import { FONT_STYLES, COLORS } from "../../constants";
+
 const CatalogItem = ({
   item: { name, price, imageSrc, numInStock, companyId },
   link,
@@ -11,23 +13,28 @@ const CatalogItem = ({
   const companies = useSelector((state) => state.companiesReducer.companies);
   const status = useSelector((state) => state.companiesReducer.status);
 
-  const getCompanyName = () => {
-    return companies.companies.find((element) => element._id === companyId)
-      .name;
+  const getCompany = () => {
+    return companies.companies.find((company) => company._id === companyId);
   };
 
   return (
     <>
       {status === "ready" ? (
-        <WrapperLink to={link}>
-          <img src={imageSrc} alt={name} />
-          <ItemName>{name}</ItemName>
+        <Wrapper>
+          <Link to={link}>
+            <img src={imageSrc} alt={name} />
+          </Link>
+          <ItemName to={link}>
+            <h3>{name}</h3>
+          </ItemName>
           <ItemInfo>
             <Price>{price}</Price>
             {/* {numInStock > 0 ? <p>In stock</p> : <p>Not in stock</p>} */}
-            <CompanyName>{getCompanyName()}</CompanyName>
+            <CompanyName to={`/companies/${getCompany()._id}`}>
+              {getCompany().name}
+            </CompanyName>
           </ItemInfo>
-        </WrapperLink>
+        </Wrapper>
       ) : (
         <Loading />
       )}
@@ -35,20 +42,19 @@ const CatalogItem = ({
   );
 };
 
-const WrapperLink = styled(Link)`
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  img {
-    height: 150px;
-  }
-
   box-sizing: border-box;
   width: 100%;
   padding: 20px;
   margin: 40px 20px 0 0;
   border-radius: 5px;
   box-shadow: 0px 10px 50px lightgrey;
+  img {
+    height: 150px;
+  }
   &:hover {
     box-shadow: 0px 10px 50px silver;
   }
@@ -60,23 +66,36 @@ const WrapperLink = styled(Link)`
   }
 `;
 
-const ItemName = styled.h3`
+const ItemName = styled(Link)`
+  font-family: ${FONT_STYLES.itemName};
+  font-weight: 700;
   font-size: 1.1em;
   line-height: 1.4;
+  color: #373737;
   padding: 20px 0 10px;
+  transition: all linear 200ms;
+  &:hover {
+    color: ${COLORS.primary};
+  }
 `;
 
 const ItemInfo = styled.div`
+  font-family: ${FONT_STYLES.itemInfo};
   width: 100%;
 `;
 
 const Price = styled.p`
   font-size: 1.1em;
-  padding: 10px 0;
+  color: #373737;
+  padding: 10px 0 20px;
 `;
 
-const CompanyName = styled.p`
-  font-style: italic;
+const CompanyName = styled(Link)`
+  color: #848484;
+  transition: all linear 200ms;
+  &:hover {
+    color: ${COLORS.primary};
+  }
 `;
 
 export default CatalogItem;
